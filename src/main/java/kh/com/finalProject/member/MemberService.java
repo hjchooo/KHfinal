4d6c5aa73d8b98285a45b316a3b9035b0b086672
jp <wlsvy1005@gmail.com>
2022-01-28 16:02:05 +0900

    joinus

--- src/main/java/kh/com/finalProject/member/MemberService.java
+++ src/main/java/kh/com/finalProject/member/MemberService.java
@@ -41,6 +41,28 @@ public class MemberService {
 		}
 	}
 
+	//	비밀번호 찾기 시 아이디 존재여부 확인
+	public boolean existID(String id) throws Exception {
+		if (dao.existID(id) == 1) {
+
+			return true;
+		} else {
+
+			return false;
+		}
+	}
+
+	//	비밀번호 찾기 시 이메일 존재여부 확인
+	public boolean existEmail(String email) throws Exception {
+		if (dao.existEmail(email) == 1) {
+
+			return true;
+		} else {
+
+			return false;
+		}
+	}
+
 	// Nickname 중복검사
 	// 중복검사 메서드는 dao안에 있음
 	public boolean verifyNickname(String nickname) throws Exception {
@@ -51,32 +73,43 @@ public class MemberService {
 		}
 	}
 
+	//	이메일 중복검사
+	public boolean verifyEmail(String email) throws Exception {
+		if (dao.verifyEmail(email) == 1) {
+
+			return false;
+		} else {
+
+			return true;
+		}
+	}
+
 	// 회원가입
 	public int insertMember(MemberDTO dto) throws Exception {
 		System.out.println("MemberService 회원가입");
 		return dao.insertMember(dto);
 	}
-	
-	//	아이디 찾기
+
+	// 아이디 찾기
 	public List<MemberDTO> findID(String email) throws Exception {
-		
+
 		return dao.findID(email);
 	}
-	
+
 	// 회원 탈퇴
-		public int delete(String id) throws Exception {
-			return dao.delete(id);
-		}
+	public int delete(String id) throws Exception {
+		return dao.delete(id);
+	}
 
-		// 회원정보 수정
-		public int toModify(MemberDTO dto) throws Exception {
-			System.out.println("toModify");
-			return dao.toModify(dto);
-		}
+	// 회원정보 수정
+	public int toModify(MemberDTO dto) throws Exception {
+		System.out.println("toModify");
+		return dao.toModify(dto);
+	}
 
-		// 회원전체 조회
-		public List<MemberDTO> selectAll() throws Exception {
-			System.out.println("note 팝업 service 도착");
-			return dao.selectAll();
-		}
+	// 회원전체 조회
+	public List<MemberDTO> selectAll() throws Exception {
+		System.out.println("note 팝업 service 도착");
+		return dao.selectAll();
+	}
 }
